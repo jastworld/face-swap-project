b84be42cbcab62b34b2c3d7afb522939f2203a68
Tayo Amuneke <tayo.amuneke@aol.com>
2023-04-30 22:08:06 -0700

    Added metrics

--- evaluation/README.md
+++ evaluation/README.md
@@ -1,4 +1,5 @@
-SSIM (Structural Similarity Index) is a widely used image quality metric that measures the similarity between two images based on their structural information. The metric was designed to model the human perception of image quality, and it is particularly useful for evaluating the performance of image processing algorithms such as image compression or image enhancement techniques.
+### SSIM (Structural Similarity Index) 
+SSIM is a widely used image quality metric that measures the similarity between two images based on their structural information. The metric was designed to model the human perception of image quality, and it is particularly useful for evaluating the performance of image processing algorithms such as image compression or image enhancement techniques.
 
 The SSIM index ranges from -1 to 1, where a score of 1 indicates that the two images are identical, and a score of -1 indicates that they are completely dissimilar. A score of 0 indicates that the two images are uncorrelated.
 
@@ -7,3 +8,9 @@ The SSIM metric consists of three components: luminance, contrast, and structure
 To calculate the SSIM index between two images, the images are first divided into small windows, and the SSIM score is calculated for each window. The window scores are then combined to produce a single SSIM score for the entire image.
 
 SSIM is a widely used metric in computer vision and image processing applications because it provides a more accurate measure of image quality than other metrics such as mean squared error (MSE) or peak signal-to-noise ratio (PSNR), which only measure the difference in pixel values between two images.
+
+### PSNR (Peak Signal-to-Noise Ratio)
+PSNR is a measure of the ratio between the maximum possible power of a signal and the power of corrupting noise that affects the quality of the signal. In image processing, it is used to measure the quality of the reconstructed image after compression or other processing. Higher PSNR values indicate higher image quality.
+
+### MSE (Mean Squared Error)
+MSE measures the average squared differences between two images. It's calculated by comparing each pixel value in the original and processed image and taking the square of the difference between them. A smaller MSE value indicates better image quality.
--- evaluation/eval.py
+++ evaluation/eval.py
@@ -1,10 +1,57 @@
 import cv2
+from skimage.metrics import structural_similarity, simple_metrics
+import os
+import numpy as np
 
-def compare_ssim(original_img, swapped_img):
+def compare_images(original_img, swapped_img):
     # Convert the images to grayscale
     original_gray = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
     swapped_gray = cv2.cvtColor(swapped_img, cv2.COLOR_BGR2GRAY)
 
-    # Calculate the SSIM between the original and swapped images
-    ssim_score = compare_ssim(original_gray, swapped_gray)
-    return ssim_score
+    ssim = structural_similarity(original_gray, swapped_gray)
+    psnr = simple_metrics.peak_signal_noise_ratio(original_gray, swapped_gray)
+    mse = simple_metrics.mean_squared_error(original_gray, swapped_gray)
+    return ssim, psnr, mse
+
+def get_images_path(dir_path):
+    paths = []
+    for filename in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, filename)):
+            paths.append(os.path.join(dir_path, filename))
+    paths.sort()
+    return paths
+
+def reshape_images(images):
+    min_shape = np.array([999999999, 999999999, 999999999])
+    for image in images:
+        mask = image.shape < min_shape
+        min_shape = image.shape * mask + min_shape * (1-mask)
+
+    new_shape = (min_shape[0], min_shape[1])
+    for i in range(len(images)):
+        images[i] = cv2.resize(images[i], new_shape)
+    return images
+
+source_directory = "./image/source"
+direct_cut_directory = "./image/result/direct_cut"
+poisson_blend_directory = "./image/result/poisson_blend"
+source_images = get_images_path(source_directory)
+direct_cut_images = get_images_path(direct_cut_directory)
+poisson_blend_images = get_images_path(poisson_blend_directory)
+
+N = len(source_images)
+assert N == len(direct_cut_images) == len(poisson_blend_images)
+
+for i in range(N):
+    images = [cv2.imread(source_images[i]), cv2.imread(direct_cut_images[i]), cv2.imread(poisson_blend_images[i])]
+    src_img, direct_cut_img, poisson_blend_img= reshape_images(images)
+    
+    assert src_img.shape == direct_cut_img.shape == poisson_blend_img.shape
+    print(i, ":", end=" ")
+    print(compare_images(src_img, direct_cut_img), end=" ")
+    print(compare_images(src_img, poisson_blend_img))
+
+
+# # Load the source and target images
+# src_img = cv2.imread("path/to/source/image.jpg")
+# tgt_img = cv2.imread("path/to/target/image.jpg")
