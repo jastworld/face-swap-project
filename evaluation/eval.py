8f2594ddfcbfe8f6f10434d80d6da32fadbfa72a
Tayo Amuneke <tayo.amuneke@aol.com>
2023-05-05 21:20:22 -0700

    Updated table

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -2,6 +2,8 @@ import cv2
 from skimage.metrics import structural_similarity, simple_metrics
 import os
 import numpy as np
+from tabulate import tabulate
+
 
 def compare_images(original_img, swapped_img):
     # Convert the images to grayscale
@@ -32,25 +34,42 @@ def reshape_images(images):
         images[i] = cv2.resize(images[i], new_shape)
     return images
 
-source_directory = "./image/source"
-direct_cut_directory = "./image/result/direct_cut"
-poisson_blend_directory = "./image/result/poisson_blend"
+source_directory = ".\image\source"
+direct_cut_directory = ".\image\\result\direct_cut"
+poisson_blend_directory = ".\image\\result\poisson_blend"
 source_images = get_images_path(source_directory)
 direct_cut_images = get_images_path(direct_cut_directory)
 poisson_blend_images = get_images_path(poisson_blend_directory)
 
 N = len(source_images)
 assert N == len(direct_cut_images) == len(poisson_blend_images)
-
+evaluation = {
+    "Images":[],
+    "Direct Cut SSIM": [],
+    "Direct Cut PSNR": [],
+    "Direct Cut MSE": [],
+    "Poisson Blend SSIM": [],
+    "Poisson Blend PSNR": [],
+    "Poisson Blend MSE": [],
+}
 for i in range(N):
     images = [cv2.imread(source_images[i]), cv2.imread(direct_cut_images[i]), cv2.imread(poisson_blend_images[i])]
+    if images[0] is None or images[1] is None or images[2] is None:
+        continue
     src_img, direct_cut_img, poisson_blend_img= reshape_images(images)
     
     assert src_img.shape == direct_cut_img.shape == poisson_blend_img.shape
-    print(i, ":", end=" ")
-    print(compare_images(src_img, direct_cut_img), end=" ")
-    print(compare_images(src_img, poisson_blend_img))
+    evaluation["Images"].append(source_images[i])
+    ssim, psnr, mse = compare_images(src_img, direct_cut_img)
+    evaluation["Direct Cut SSIM"].append(ssim)
+    evaluation["Direct Cut PSNR"].append(psnr)
+    evaluation["Direct Cut MSE"].append(mse)
+    ssim, psnr, mse = compare_images(src_img, poisson_blend_img)
+    evaluation["Poisson Blend SSIM"].append(ssim)
+    evaluation["Poisson Blend PSNR"].append(psnr)
+    evaluation["Poisson Blend MSE"].append(mse)
 
+print(tabulate(evaluation, headers='keys'))
 
 # # Load the source and target images
 # src_img = cv2.imread("path/to/source/image.jpg")
