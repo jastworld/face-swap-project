0e1bf75c273bbfb8f6ba5bce2d493f2418e9d814
Tayo Amuneke <tayo.amuneke@gmail.com>
2023-05-06 22:55:39 -0700

    Added main function

--- evaluation/evaluation_script.py
+++ evaluation/evaluation_script.py
@@ -87,60 +87,60 @@ def reshape_images(images):
         images[i] = cv2.resize(images[i], new_shape)
     return images
 
+if __name__ == "__main__":
+    source_directory = ".\image\source"
+    target_directory = ".\image\\target"
+    direct_cut_directory = ".\image\\result\direct_cut_source_swap"
+    poisson_blend_directory = ".\image\\result\poisson_blend_source_swap"
+    source_images = get_images_path(source_directory)
+    target_images = get_images_path(target_directory)
 
-source_directory = ".\image\source"
-target_directory = ".\image\\target"
-direct_cut_directory = ".\image\\result\direct_cut_source_swap"
-poisson_blend_directory = ".\image\\result\poisson_blend_source_swap"
-source_images = get_images_path(source_directory)
-target_images = get_images_path(target_directory)
+    direct_cut_images = get_images_path(direct_cut_directory)
+    poisson_blend_images = get_images_path(poisson_blend_directory)
+    print(direct_cut_images)
 
-direct_cut_images = get_images_path(direct_cut_directory)
-poisson_blend_images = get_images_path(poisson_blend_directory)
-print(direct_cut_images)
+    N = len(target_images)
+    print(N, len(direct_cut_images), len(poisson_blend_images))
+    assert N == len(direct_cut_images) == len(poisson_blend_images)
+    evaluation = {
+        "Images": [],
+        "Direct Cut SSIM": [],
+        "Direct Cut PSNR": [],
+        "Direct Cut MSE": [],
+        "Poisson Blend SSIM": [],
+        "Poisson Blend PSNR": [],
+        "Poisson Blend MSE": [],
+    }
+    for i in range(N):
+        if target_images[i].split("\\")[-1] == direct_cut_images[i].split("\\")[-1] ==  poisson_blend_images[i].split("\\")[-1]:
+            pass
+        else:
+            continue
+        images = [cv2.imread(target_images[i]), cv2.imread(direct_cut_images[i]), cv2.imread(poisson_blend_images[i])]
+        if images[0] is None or images[1] is None or images[2] is None:
+            continue
+        src_img, direct_cut_img, poisson_blend_img = reshape_images(images)
 
-N = len(target_images)
-print(N, len(direct_cut_images), len(poisson_blend_images))
-assert N == len(direct_cut_images) == len(poisson_blend_images)
-evaluation = {
-    "Images": [],
-    "Direct Cut SSIM": [],
-    "Direct Cut PSNR": [],
-    "Direct Cut MSE": [],
-    "Poisson Blend SSIM": [],
-    "Poisson Blend PSNR": [],
-    "Poisson Blend MSE": [],
-}
-for i in range(N):
-    if target_images[i].split("\\")[-1] == direct_cut_images[i].split("\\")[-1] ==  poisson_blend_images[i].split("\\")[-1]:
-        pass
-    else:
-        continue
-    images = [cv2.imread(target_images[i]), cv2.imread(direct_cut_images[i]), cv2.imread(poisson_blend_images[i])]
-    if images[0] is None or images[1] is None or images[2] is None:
-        continue
-    src_img, direct_cut_img, poisson_blend_img = reshape_images(images)
+        assert src_img.shape == direct_cut_img.shape == poisson_blend_img.shape
+        ssim, psnr, mse = compare_images(src_img, direct_cut_img)
+        if ssim == 1: # skip cos it adds noise
+            continue
+        evaluation["Images"].append(target_images[i].split("\\")[-1])
 
-    assert src_img.shape == direct_cut_img.shape == poisson_blend_img.shape
-    ssim, psnr, mse = compare_images(src_img, direct_cut_img)
-    if ssim == 1: # skip cos it adds noise
-        continue
-    evaluation["Images"].append(target_images[i].split("\\")[-1])
+        evaluation["Direct Cut SSIM"].append(ssim)
+        evaluation["Direct Cut PSNR"].append(psnr)
+        evaluation["Direct Cut MSE"].append(mse)
+        ssim, psnr, mse = compare_images(src_img, poisson_blend_img)
+        evaluation["Poisson Blend SSIM"].append(ssim)
+        evaluation["Poisson Blend PSNR"].append(psnr)
+        evaluation["Poisson Blend MSE"].append(mse)
 
-    evaluation["Direct Cut SSIM"].append(ssim)
-    evaluation["Direct Cut PSNR"].append(psnr)
-    evaluation["Direct Cut MSE"].append(mse)
-    ssim, psnr, mse = compare_images(src_img, poisson_blend_img)
-    evaluation["Poisson Blend SSIM"].append(ssim)
-    evaluation["Poisson Blend PSNR"].append(psnr)
-    evaluation["Poisson Blend MSE"].append(mse)
+    evaluation["Images"].append("Average")
 
-evaluation["Images"].append("Average")
-
-evaluation["Direct Cut SSIM"].append(np.average(evaluation["Direct Cut SSIM"]))
-evaluation["Direct Cut PSNR"].append(np.average(evaluation["Direct Cut PSNR"]))
-evaluation["Direct Cut MSE"].append(np.average(evaluation["Direct Cut MSE"]))
-evaluation["Poisson Blend SSIM"].append(np.average(evaluation["Poisson Blend SSIM"]))
-evaluation["Poisson Blend PSNR"].append(np.average(evaluation["Poisson Blend PSNR"]))
-evaluation["Poisson Blend MSE"].append(np.average(evaluation["Poisson Blend MSE"]))
-print(tabulate(evaluation, headers='keys', tablefmt="pipe"))
+    evaluation["Direct Cut SSIM"].append(np.average(evaluation["Direct Cut SSIM"]))
+    evaluation["Direct Cut PSNR"].append(np.average(evaluation["Direct Cut PSNR"]))
+    evaluation["Direct Cut MSE"].append(np.average(evaluation["Direct Cut MSE"]))
+    evaluation["Poisson Blend SSIM"].append(np.average(evaluation["Poisson Blend SSIM"]))
+    evaluation["Poisson Blend PSNR"].append(np.average(evaluation["Poisson Blend PSNR"]))
+    evaluation["Poisson Blend MSE"].append(np.average(evaluation["Poisson Blend MSE"]))
+    print(tabulate(evaluation, headers='keys', tablefmt="pipe"))
