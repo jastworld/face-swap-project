9330853e2c3c16ba7887125edc166185254b7daa
Tayo Amuneke <tayo.amuneke@gmail.com>
2023-05-06 22:43:50 -0700

    Updated SSIM

--- evaluation/evaluation_script.py
+++ evaluation/evaluation_script.py
@@ -1,18 +1,69 @@
+import math
+
 import cv2
-from skimage.metrics import structural_similarity, mean_squared_error
 import os
 import numpy as np
 from tabulate import tabulate
 
+
+def ssim_fn(img1, img2):
+    C1 = (0.01 * 255)**2
+    C2 = (0.03 * 255)**2
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1**2
+    mu2_sq = mu2**2
+    mu1_mu2 = mu1 * mu2
+
+    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+def calculate_psnr(img1, img2):
+    # img1 and img2 have range [0, 255]
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    mse = np.mean((img1 - img2)**2)
+    if mse == 0:
+        return float('inf')
+    return 20 * math.log10(255.0 / math.sqrt(mse)), mse
+
+def calculate_ssim(img1, img2):
+    '''calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    '''
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    if img1.ndim == 2:
+        return ssim_fn(img1, img2)
+    elif img1.ndim == 3:
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim(img1, img2))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
 
 def compare_images(original_img, swapped_img):
     # Convert the images to grayscale
     original_gray = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
     swapped_gray = cv2.cvtColor(swapped_img, cv2.COLOR_BGR2GRAY)
 
-    ssim = structural_similarity(original_gray, swapped_gray)
-    mse = mean_squared_error(original_gray, swapped_gray) + 1e-10
-    psnr = 10 * np.log10(255**2 / mse)
+    ssim = calculate_ssim(original_gray, swapped_gray)
+    psnr, mse = calculate_psnr(original_gray, swapped_gray)
     return ssim, psnr, mse
 
 
@@ -62,7 +113,7 @@ evaluation = {
 }
 for i in range(N):
     if target_images[i].split("\\")[-1] == direct_cut_images[i].split("\\")[-1] ==  poisson_blend_images[i].split("\\")[-1]:
-        print(target_images[i].split("\\")[-1])
+        pass
     else:
         continue
     images = [cv2.imread(target_images[i]), cv2.imread(direct_cut_images[i]), cv2.imread(poisson_blend_images[i])]
